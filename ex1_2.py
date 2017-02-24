731cec93dfc3886ac3fcf6397e1dce18770b16fd
giraffegzy <271349505@qq.com>
2017-02-24 01:09:24 -0600

    Add files via upload

---
+++ ex1_2.py
@@ -0,0 +1,35 @@
+import postgresql
+import postgresql.driver as pg_driver
+import uuid
+
+
+db = pg_driver.connect(user = 'postgres',password = '271349505',host = '127.0.0.1',
+port = 5432,
+database = 'shop'
+)
+
+def create_code(n, length):   #生成”n“个激活码，每个激活码含有”length“位
+    result = []
+
+    while True:
+        jihuoma = uuid.uuid4()
+        list = str(jihuoma).replace('-', '')[:length]
+        if not list in result:
+            result.append(list)
+        if len(result) == n:
+            break
+    return result
+temp = (create_code(10, 8))
+print(temp)
+
+
+for c in range(0, 10):
+    a = temp[c]
+    sql_a = "insert into pyex5(ide) values ('{}')".format(a)
+    try:
+        db.query(sql_a)
+    except postgresql.exceptions.UniqueError:
+        pass
+
+
+
